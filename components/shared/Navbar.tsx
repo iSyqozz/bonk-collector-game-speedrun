58a219e845f29d0142a6550c33a063c36ce8f63c
Fares Hassen <fares.hassen512@gmail.com>
2023-12-17 06:23:45 +0100

    nav opacity

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <></>
+    <div className='text-white text-opacity-50 text-[10px] w-ful flex items-center justify-start p-1 '>
+      made under 24 hours 😅
+    </div>
   )
 }
 
